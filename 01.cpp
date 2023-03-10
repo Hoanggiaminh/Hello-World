b47db8b3c8f86b5d30ff05fd9f78c9ff7fb1563b
Hoanggiaminh <127482516+Hoanggiaminh@users.noreply.github.com>
2023-03-10 20:33:06 +0700

    Update 01.cpp
    This is my first change

--- 01.cpp
+++ 01.cpp
@@ -4,16 +4,6 @@
 
 using namespace std;
 
-/*int main() {
-    int n,nam=0,tuan=0,ngay=0;
-    cin >> n;
-    nam=n/365;
-    tuan=(n-365*nam)/7;
-    ngay=n-365*nam-7*tuan;
-    cout << nam << tuan << ngay;
-    return 0;
-}*/
-
 int main()
 {
     float a,b,c,tam;
